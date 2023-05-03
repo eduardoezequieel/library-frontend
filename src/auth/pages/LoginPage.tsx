888e2c3624284901800987ab7aa3da87ef07114d
Eduardo Ezequiel <riv.edu10@gmail.com>
2023-05-03 15:42:22 -0600

    :lock: Add validations

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,7 +1,30 @@
+import classNames from "classnames";
+import { useForm } from "react-hook-form";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginForm>({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="md:w-1/4 sm:w-1/2 w-[80%] flex flex-col gap-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="min-[450px]:w-[300px] w-[80%] flex flex-col gap-2"
+      >
         <h1 className="text-center text-2xl font-bold">Sign in</h1>
         <p className="text-center text-neutral-400 font-heading font-bold">
           My U Library
@@ -9,24 +32,38 @@ const LoginPage = () => {
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
-            className="form-control"
+            className={classNames("form-control", {
+              "!border-red-500": errors.email,
+            })}
             placeholder="email@example.com"
             type="text"
-            name="email"
-            id="email"
+            {...register("email", {
+              required: "This field is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Please enter a valid email",
+              },
+            })}
           />
+          {errors.email && (
+            <p className="text-sm text-red-500">{errors.email.message}</p>
+          )}
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
-            className="form-control"
+            className={classNames("form-control ", {
+              "!border-red-500": errors.password,
+            })}
             type="password"
-            name="password"
-            id="password"
             placeholder="Password"
+            {...register("password", {
+              required: "This field is required",
+            })}
           />
+          {<p className="text-sm text-red-500">{errors.password?.message}</p>}
         </div>
-        <button type="submit" className="primary-button">
+        <button type="submit" disabled={!isValid} className="primary-button">
           Log in
         </button>
       </form>
